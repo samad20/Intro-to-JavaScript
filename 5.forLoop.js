62cff32eb22b7b26820fe4bc837137c239e5503b
samad20 <musab.aljahdhami@gmail.com>
2022-03-15 04:57:23 -0400

    forLoop

---
+++ 5.forLoop.js
@@ -0,0 +1,11 @@
+let result = 1;
+for (let counter = 0; counter < 10; counter = counter + 1) {
+    result = result * 2;
+}
+console.log(result);
+// → 1024”
+
+// Excerpt From
+// Eloquent JavaScript: A Modern Introduction to Programming
+// Marijn Haverbeke
+// This material may be protected by copyright.
