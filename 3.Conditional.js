796e98f8765dd7ff24db5c163017405db3d6e037
samad20 <musab.aljahdhami@gmail.com>
2022-03-15 04:43:37 -0400

    Conditional

--- 3.Conditional.js
+++ 3.Conditional.js
@@ -1,7 +1,25 @@
+if (1 + 1 == 2)
+    console.log("It's true");
+// → It's true
+
+
 let theNumber = Number(prompt("Pick a number"));
+
 if (!Number.isNaN(theNumber)) {
     console.log("Your number is the square root of " + theNumber * theNumber);
 }
+else {
+    console.log("Hey. Why didn't you give me a number?");
+}
+
+let num = Number(prompt("Pick a number"));
+if (num < 10) {
+    console.log("Small");
+}
+else if (num < 100) {
+    console.log("Medium");
+}
+else { console.log("Large"); }
 
 // Excerpt From
 // Eloquent JavaScript: A Modern Introduction to Programming
