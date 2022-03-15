502e89e43ea1723afb712ce802b65acf79752cff
samad20 <musab.aljahdhami@gmail.com>
2022-03-15 04:31:42 -0400

    Conditional

---
+++ Conditional.js
@@ -0,0 +1,9 @@
+let theNumber = Number(prompt("Pick a number"));
+if (!Number.isNaN(theNumber)) {
+    console.log("Your number is the square root of " + theNumber * theNumber);
+}
+
+// Excerpt From
+// Eloquent JavaScript: A Modern Introduction to Programming
+// Marijn Haverbeke
+// This material may be protected by copyright.
