f7baf31f316bfda3374c253093f200986d81da2c
samad20 <musab.aljahdhami@gmail.com>
2022-03-15 04:06:46 -0400

    Bindings

---
+++ Bindings.js
@@ -0,0 +1,24 @@
+// Excerpt From
+// Eloquent JavaScript: A Modern Introduction to Programming
+// Marijn Haverbeke
+// This material may be protected by copyright.
+
+let num = 10;
+console.log(num * num);
+// → 100
+
+let mood = "light";
+console.log(mood);
+// → lightmood = "dark";console.log(mood);// → dark”
+
+let one = 1, two = 2;
+console.log(one + two);
+// → 3”
+
+var name = "Ayda";  //varible not used much
+const greeting = "Hello ";  //constant
+console.log(greeting + name);
+// → Hello Ayda”
+
+
+// “The collection of bindings and their values that exist at a given time is called the environment.”
