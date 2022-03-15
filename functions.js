15af46269d4ac0dff258c5aa35c645310612af9e
samad20 <musab.aljahdhami@gmail.com>
2022-03-15 04:22:45 -0400

    functions

---
+++ functions.js
@@ -0,0 +1,14 @@
+//console.log used for print contact
+
+console.log('hello world')
+
+console.log(Math.max(2, 4));
+// → 4”
+
+let theNumber = Number(prompt("Pick a number"));
+console.log("Your number is the square root of " + theNumber * theNumber);
+
+// Excerpt From
+// Eloquent JavaScript: A Modern Introduction to Programming
+// Marijn Haverbeke
+// This material may be protected by copyright.
